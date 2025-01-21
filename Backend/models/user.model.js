d3d0114490407b7bfd4135ba6cf6425cc58e1f5b
Purvesh Mali <purveshmali99@gmail.com>
2025-01-21 20:23:02 +0530

    user authentication done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -31,8 +31,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
-};
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+  return token;
+}
 
 userSchema.methods.comparePassword = async function (password, hashedPassword) {
   return await bcrypt.compare(password, hashedPassword);
