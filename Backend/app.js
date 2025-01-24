58b2d47fa66fde63b0d5b4c28665dd03ff1ca08f
Purvesh Mali <purveshmali99@gmail.com>
2025-01-24 14:18:41 +0530

    register captain done

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
-
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
@@ -15,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);  
 
 module.exports = app;
